16f6057693e5cab4258c781a3d81ed779c62dfda
w7DVL-lopez17 <w7DVL-lopez17@w7DVL-lopez17PC>
2017-06-18 18:58:11 +0200

    Versó 0.2 del projecte prjava01lopez17

--- src/prjava01lopez17/Prjava01lopez17.java
+++ src/prjava01lopez17/Prjava01lopez17.java
@@ -5,18 +5,20 @@ public class Prjava01lopez17 {
 
     public static void main(String[] args) throws IOException {
        
-       File f = new File("fitxer01lopez17.com");
+       File f = new File("fitxer01lopez17.html");
        
        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
            
            bw.write("<html>");bw.newLine();
            bw.write("<head>");bw.newLine();
            bw.write("<title>");bw.newLine();
-           bw.write("Nova página web de lopez17");bw.newLine();
+           bw.write("Nova p$agrave;gina web de lopez17");bw.newLine();
            bw.write("</title>");bw.newLine();
            bw.write("</head>");bw.newLine();
            bw.write("<body>");bw.newLine();
-           bw.write("Nova página web de lopez17");bw.newLine();
+           bw.write("Nova p$agrave;gina web de lopez17");bw.newLine();
+           bw.write("<br>");bw.newLine();
+           bw.write("DAW2 UF4 Pr$agrave;ctica 2");bw.newLine();
            bw.write("</body>");bw.newLine();
            bw.write("</html>");bw.newLine();
            bw.close();
